e27b55d9cd9c4ef6bfb7ad4987cc6171232ead4e
denniswaituika <40142465+denniswaituika@users.noreply.github.com>
2024-11-17 23:48:43 +0300

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -22,8 +22,12 @@ from . import views
 urlpatterns = [
     path('book_list/', views.book_list, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', views.register, name='register'),
     path('login/', views.login_view.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', views.logout_view.as_view(), name='logout'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-from django.views.generic import TemplateView
 from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
+
 
 def book_list(request):
       """Retrieves all books and renders a template displaying the list."""
@@ -31,23 +34,39 @@ class LibraryDetailView(DetailView):
 
 def login_view(request):
     return auth_views.LoginView.as_view(template_name="relationship_app/login.html")(
-        request
+      request
     )
 
 
 def logout_view(request):
-    return auth_views.LogoutView.as_view(template_name="relationship_app/logout.html")(
-        request
+  return auth_views.LogoutView.as_view(template_name="relationship_app/logout.html")(
+    request
     )
 
 
 def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("home")  # Redirect to a home page after registration
-    else:
-        form = UserCreationForm()
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect("home")  # Redirect to a home page after registration
+  else:
+    form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
+  
+  def check_role(user, role):
+    return user.userprofile.role == role
+
+@user_passes_test(lambda u: check_role(u, 'Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(lambda u: check_role(u, 'Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(lambda u: check_role(u, 'Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
